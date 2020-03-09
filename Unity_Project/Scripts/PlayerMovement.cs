e9d51c30cece593db74133e02fe5eb5fa68b7959
SlashCrash <45591005+SlashCrash@users.noreply.github.com>
2020-03-09 19:45:41 +0100

    Initial Basis for WaterDrop Project
    Add main assets

---
+++ Unity_Project/Scripts/PlayerMovement.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float moveSpeed;
+    public float jumpForce;
+
+    public bool isJumping;
+    public bool isGrounded;
+
+
+    public Transform groundCheckLeft;
+    public Transform groundCheckRight;
+
+    public Rigidbody2D rigidB;
+    private Vector3 velocity = Vector3.zero;
+
+
+
+    private void FixedUpdate()
+    {
+        isGrounded = Physics2D.OverlapArea(groundCheckLeft.position, groundCheckRight.position);
+        float horizontalMovement = Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime;
+
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            isJumping = true;
+        }
+        MovePlayer(horizontalMovement);
+    }
+
+    void MovePlayer(float _horizontalMovement)
+    {
+        Vector3 targetVelocity = new Vector2(_horizontalMovement, rigidB.velocity.y);
+        rigidB.velocity = Vector3.SmoothDamp(rigidB.velocity, targetVelocity, ref velocity, .05f);
+
+        if (isJumping == true)
+        {
+            rigidB.AddForce(new Vector2(0f, jumpForce));
+            isJumping = false;
+        }
+    }
+}
